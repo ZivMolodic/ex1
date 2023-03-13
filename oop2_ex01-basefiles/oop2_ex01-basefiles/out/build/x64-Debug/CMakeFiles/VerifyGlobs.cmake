36121c049b2a92985a1156b54dac37da85eacd7a
Ziv Molodic <kashi.ziv@gmail.com>
2023-03-13 22:29:00 +0200

    first commit of ziv

--- oop2_ex01-basefiles/oop2_ex01-basefiles/out/build/x64-Debug/CMakeFiles/VerifyGlobs.cmake
+++ oop2_ex01-basefiles/oop2_ex01-basefiles/out/build/x64-Debug/CMakeFiles/VerifyGlobs.cmake
@@ -5,6 +5,7 @@ cmake_policy(SET CMP0009 NEW)
 # MY_HEADER_FILES at include/CMakeLists.txt:2 (file)
 file(GLOB NEW_GLOB LIST_DIRECTORIES false RELATIVE "C:/Users/kashi/Documents/OOP2/ex1/oop2_ex01-basefiles/oop2_ex01-basefiles/include" "C:/Users/kashi/Documents/OOP2/ex1/oop2_ex01-basefiles/oop2_ex01-basefiles/include/*.h")
 set(OLD_GLOB
+  "FuncCalculator.h"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
@@ -14,6 +15,7 @@ endif()
 # MY_SOURCE_FILES at src/CMakeLists.txt:1 (file)
 file(GLOB_RECURSE NEW_GLOB LIST_DIRECTORIES false RELATIVE "C:/Users/kashi/Documents/OOP2/ex1/oop2_ex01-basefiles/oop2_ex01-basefiles/src" "C:/Users/kashi/Documents/OOP2/ex1/oop2_ex01-basefiles/oop2_ex01-basefiles/src/*.cpp")
 set(OLD_GLOB
+  "FuncCalculator.cpp"
   "main.cpp"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
